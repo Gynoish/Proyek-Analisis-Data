b6f3891f834b43fcc5bf7490a6739d54dad05f8a
Gynoish <danishdhiyaulhaq6@gmail.com>
2024-10-06 20:40:42 +0700

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -6,21 +6,43 @@ import streamlit as st
 from babel.numbers import format_currency
 sns.set(style='darkgrid')
 
-day_df = pd.read_csv("day.csv")
-hour_df = pd.read_csv("hour.csv")
+day_df = pd.read_csv("dashboard/day.csv")
+hour_df = pd.read_csv("dashboard/hour.csv")
 
 st.title("Bike Sharing Dashboard")
 
+hour_df['dteday'] = pd.to_datetime(hour_df['dteday'])
+
+selected_date = st.date_input("Pilih Tanggal", value=pd.to_datetime("2011-01-01"), min_value=hour_df['dteday'].min(), max_value=hour_df['dteday'].max())
+
+filtered_data = hour_df[hour_df['dteday'] == pd.to_datetime(selected_date)]
+
+st.subheader("Penggunaan Sepeda Berdasarkan Jam")
+
+hourly_summary = filtered_data.groupby('hr')['cnt'].mean().reset_index()
+
+plt.figure(figsize=(10, 6))
+sns.lineplot(x='hr', y='cnt', data=hourly_summary, marker='o', palette="Blues")
+plt.title('Rata-rata Penyewaan Sepeda Berdasarkan Jam')
+plt.xlabel('Jam')
+plt.ylabel('Rata-rata Penyewaan Sepeda')
+st.pyplot(plt)
+
+season_labels = {1: "Springer", 2: "Summer", 3: "Fall", 4: "Winter"}
+
+hour_df['season'] = hour_df['season'].map(season_labels)
+
 grouped_data = hour_df.groupby(by="season").agg({
     "cnt": "mean"
-})
+}).reset_index()
 
 sns.barplot(x='season', y='cnt', data=grouped_data)
-plt.xlabel('Cuaca')  
+plt.xlabel('Musim')  
 plt.ylabel('Rata-rata penyewaan sepeda') 
 st.pyplot(plt)
 
-st.write("Pada visualisasi tersebut, saya dapat melihat bahwa musim 3(fall) cenderung memiliki jumlah penyewaan yang tinggi, diikuti oleh musim 2,4, dan 1")
+
+st.write("Pada visualisasi tersebut, saya dapat melihat bahwa musim fall atau gugur cenderung memiliki jumlah penyewaan yang tinggi, diikuti oleh summer, winter dan terakhir springer")
 
 season_colors = {1: '#e6194B', 2: '#3cb44b', 3: '#ffe119', 4: '#4363d8'}
 season_labels = {1: 'springer', 2: 'summer', 3: 'fall', 4: 'winter'}
